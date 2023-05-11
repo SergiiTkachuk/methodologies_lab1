9c31b528256265d1d63eaf106a30c91ee744c112
Serhii Tkachuk <sergiytkachuk29@gmail.com>
2023-05-11 17:28:20 +0300

    Interactive mode is implemented

---
+++ src/InteractiveMode.js
@@ -0,0 +1,39 @@
+import { QuadraticSolver } from './QuadraticSolver.js';
+import { checkNumberIsNotDecimal } from './validator.js';
+
+
+const startInteractiveMode = () => {
+    const questions = [
+        'a = ',
+        'b = ',
+        'c = '
+    ];
+    const params = [];
+
+    process.stdin.on('data', (data) => {
+        if (params.length < questions.length) {
+            const parsedData = parseFloat(data.toString());
+            if (isNaN(parsedData) || checkNumberIsNotDecimal(data)) {
+                process.stdout.write(`Error: expected a real number, got ${data.toString()}`);
+                process.stdout.write(questions[params.length]);
+            } else if (parsedData === 0 && params.length === 0) {
+                process.stdout.write(`Error: a cannot be 0\n`);
+                process.stdout.write(questions[0]);
+            } else {
+                params.push(parsedData);
+                if (params.length < questions.length) {
+                    process.stdout.write(questions[params.length]);
+                } else {
+                    QuadraticSolver(...params);
+                    process.exit();
+                }
+            } 
+        }
+    });
+
+    process.stdout.write(questions[0]);
+
+};
+
+
+export { startInteractiveMode };
